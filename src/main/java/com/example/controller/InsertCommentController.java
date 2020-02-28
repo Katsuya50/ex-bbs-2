abf921464e228058668c43c9af51817587a71605
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-28 14:04:30 +0900

    軽微な修正

--- src/main/java/com/example/controller/InsertCommentController.java
+++ src/main/java/com/example/controller/InsertCommentController.java
@@ -9,10 +9,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.cotroller.ShowBbsController;
 import com.example.domain.Comment;
 import com.example.form.CommentForm;
-import com.example.service.ArticleService;
 import com.example.service.CommentService;
 
 @Controller
