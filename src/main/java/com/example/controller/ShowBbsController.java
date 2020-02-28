d85d5279a6d8914a1ea6a86096fa4c54c8230e93
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-28 14:15:26 +0900

    フォームをリクエストスコープに格納するメソッドを追加しました。

--- src/main/java/com/example/controller/ShowBbsController.java
+++ src/main/java/com/example/controller/ShowBbsController.java
@@ -7,9 +7,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Article;
+import com.example.form.ArticleForm;
 import com.example.service.ArticleService;
 
 @Controller
@@ -18,6 +20,16 @@ public class ShowBbsController {
 
 	@Autowired
 	private ArticleService articleService;
+	
+	/**
+	 * 記事のフォームを初期化します.
+	 * 
+	 * @return 記事フォーム
+	 */
+	@ModelAttribute
+	public ArticleForm setUpArticleForm() {
+		return new ArticleForm();
+	}
 	
 	
 	@RequestMapping("")
@@ -40,6 +52,6 @@ public class ShowBbsController {
 		Long lapTime = ChronoUnit.MILLIS.between(time, LocalDateTime.now());
 		model.addAttribute("lapTime", lapTime);
 		
-		return "/bbsview";
+		return "bbs";
 	}
 }
